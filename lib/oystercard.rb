a89b6d5b4bb7c1a94a1d75d05994c8ffee65aa4a
Mayo-Theodore <mayoelith@gmail.com>
2021-11-22 15:34:41 +0000

    test top_up method

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,2 +1,20 @@
 class Oystercard
+  attr_reader :balance
+  
+  DEFAULT_BALANCE = 0
+  LIMIT = 90
+  
+  def initialize(balance = DEFAULT_BALANCE)
+    @balance = balance
+  end
+
+  def top_up(value)
+    raise "The limit is £#{LIMIT}" if value > LIMIT
+      value
+  end
+
+  def deduct(trip)
+    
+  end
+
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,5 +1,30 @@
 require 'oystercard'
 
 describe Oystercard do
+  it 'checks balance' do
+    oyster = Oystercard.new
+    expect(oyster.balance).to eq 0
+  end
   
+  describe '#top_up' do
+    it 'Add money to card' do
+      oyster = Oystercard.new
+      expect(oyster.top_up(5)).to eq 5
+    end
+  end
+
+    it 'Check users cannot deposit more than limit' do
+      oyster = Oystercard.new
+      expect {oyster.top_up(91)}.to raise_error("The limit is £90")
+    end
+  
+    describe '#deduct' do
+      it 'Charges users after a trip' do
+        oyster = Oystercard.new
+        oyster.balance
+        oyster.top_up(5)
+        oyster.deduct(3)
+      end
+    end
+
 end
